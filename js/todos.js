9002b43697729282bb30bec362eee3fd018fc08d
Chris Davies <chris.davies@theironyard.com>
2015-05-21 10:41:45 -0400

    Remove todo

--- js/todos.js
+++ js/todos.js
@@ -4,10 +4,23 @@ $(function () {
     e.preventDefault();
 
     var txt = $(this).find('.todo-txt');
+    var li = $('<li class="todo-item"><span class="todo-label"></span><button class="todo-remove">X</button></li>');
 
-    $('.todo-list').append($('<li class="todo-item"></li>').text(txt.val()));
+    li.find('.todo-label').text(txt.val());
+
+    //// This is a way to handle remove... There's another
+    // li.find('.todo-remove').on('click', function () {
+    //   li.remove();
+    // });
+
+    $('.todo-list').append(li);
 
     txt.val('');
   });
+
+  $('.todo-list').on('click', '.todo-remove', function (e) {
+    $(e.target).closest('.todo-item').remove();
+  });
+
 
 });
