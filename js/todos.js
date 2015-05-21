132f3cf5f8b1921a96f3646b9ec55234185fa9ef
Chris Davies <chris.davies@theironyard.com>
2015-05-21 10:30:34 -0400

    Ability to add todos

--- js/todos.js
+++ js/todos.js
@@ -1,3 +1,13 @@
 $(function () {
-  console.log('Loaded...');
+
+  $('.todo-form').on('submit', function (e) {
+    e.preventDefault();
+
+    var txt = $(this).find('.todo-txt');
+
+    $('.todo-list').append($('<li class="todo-item"></li>').text(txt.val()));
+
+    txt.val('');
+  });
+
 });
